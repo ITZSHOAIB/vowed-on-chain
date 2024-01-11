280081518a6f40cf16aef06f2b88bfaa766acce5
ITZSHOAIB <skshoaibpersonal@gmail.com>
2024-01-11 15:03:06 +0530

    polygon mumbai testnet deployment added

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,8 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  defaultNetwork: "mumbai",
+  networks: {
+    hardhat: {},
+    mumbai: {
+      url: process.env.POLYGON_MUMBAI_RPC_PROVIDER,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+  },
 };
 
 export default config;
